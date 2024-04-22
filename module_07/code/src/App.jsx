a1a7bf365be2cefbec354e8787752f469b582037
Brandon-Ritchie <brandon.ritchie@blastpoint.com>
2024-04-22 12:02:01 -0400

    "Finish" lesson 10 from module 8

--- module_07/code/src/App.jsx
+++ module_07/code/src/App.jsx
@@ -1,37 +1,35 @@
-import * as d3 from "d3"
-import React, { useEffect, useRef, useState } from "react"
-import { getScatterData, getTimelineData } from "./utils/dummyData"
+import * as d3 from "d3";
+import React, { useEffect, useRef, useState } from "react";
+import { getScatterData, getTimelineData } from "./utils/dummyData";
 
-import Histogram from "./Histogram"
-import ScatterPlot from "./ScatterPlot"
-import Timeline from "./Timeline"
-// import Timeline from "./completed/Timeline"
-// import ScatterPlot from "./completed/ScatterPlot"
-// import Histogram from "./completed/Histogram"
+// import Histogram from "./Histogram"
+// import ScatterPlot from "./ScatterPlot"
+// import Timeline from "./Timeline"
+import Histogram from "./completed/Histogram";
+import ScatterPlot from "./completed/ScatterPlot";
+import Timeline from "./completed/Timeline";
 
-import "./styles.css"
+import "./styles.css";
 
-const parseDate = d3.timeParse("%m/%d/%Y")
-const dateAccessor = d => parseDate(d.date)
-const temperatureAccessor = d => d.temperature
-const humidityAccessor = d => d.humidity
+const parseDate = d3.timeParse("%m/%d/%Y");
+const dateAccessor = (d) => parseDate(d.date);
+const temperatureAccessor = (d) => d.temperature;
+const humidityAccessor = (d) => d.humidity;
 
 const getData = () => ({
   timeline: getTimelineData(),
   scatter: getScatterData(),
-})
+});
 const App = () => {
-  const [data, setData] = useState(getData())
+  const [data, setData] = useState(getData());
 
   useInterval(() => {
-    setData(getData())
-  }, 4000)
+    setData(getData());
+  }, 4000);
 
   return (
     <div className="App">
-      <h1>
-        Weather Dashboard
-      </h1>
+      <h1>Weather Dashboard</h1>
       <div className="App__charts">
         <Timeline
           data={data.timeline}
@@ -53,11 +51,10 @@ const App = () => {
         />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
-
+export default App;
 
 function useInterval(callback, delay) {
   const savedCallback = useRef();
